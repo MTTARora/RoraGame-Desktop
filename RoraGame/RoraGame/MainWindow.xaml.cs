7a3a30a47635dc18318e01826e0198e84bc6bf8b
hoangharora <hoangha.rora@gmail.com>
2019-02-22 04:58:20 +0700

    Update Left Menu Animation

--- RoraGame/RoraGame/MainWindow.xaml.cs
+++ RoraGame/RoraGame/MainWindow.xaml.cs
@@ -59,17 +59,6 @@ namespace RoraGame
         #endregion Minimize to tray system
 
         #region Left Side Menu
-        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonCloseMenu.Visibility = Visibility.Visible;
-            ButtonOpenMenu.Visibility = Visibility.Collapsed;
-        }
-
-        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonCloseMenu.Visibility = Visibility.Collapsed;
-            ButtonOpenMenu.Visibility = Visibility.Visible;
-        }
 
         private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
