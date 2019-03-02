75310da4237bf53734c54f0026be02b0263f849e
anhmairora <anhmai.rora@gmail.com>
2019-03-02 18:29:37 +0700

    Add news model

--- RoraGame/RoraGame/Models/News.cs
+++ RoraGame/RoraGame/Models/News.cs
@@ -8,5 +8,18 @@ namespace RoraGame.Models
 {
     class News
     {
+
+        public static List<Games> getNews()
+        {
+            List<Games> items = new List<Games>();
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Assassin's Creed", RequiredLvl = "Level 6", Description = "https://www.assassinscreed.com" });
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Call Of Duty", RequiredLvl = "Level 6", Description = "https://www.assassinscreed.com" });
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Fornite", RequiredLvl = "Level 6", Description = "https://www.assassinscreed.com" });
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Battlefield", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè " });
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Dota", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè " });
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Counter-Strike : Global Offensive", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè " });
+
+            return items;
+        }
     }
 }
