f868614372fea00b18be47bd65a8ee3c4e4a1208
anhmairora <anhmai.rora@gmail.com>
2019-03-02 18:35:24 +0700

    Init branch

--- RoraGame/RoraGame/Models/News.cs
+++ RoraGame/RoraGame/Models/News.cs
@@ -15,7 +15,7 @@ namespace RoraGame.Models
             items.Add(new Games() { Dates = "29/12/1999", Name = "Assassin's Creed", RequiredLvl = "Level 6", Description = "https://www.assassinscreed.com" });
             items.Add(new Games() { Dates = "29/12/1999", Name = "Call Of Duty", RequiredLvl = "Level 6", Description = "https://www.assassinscreed.com" });
             items.Add(new Games() { Dates = "29/12/1999", Name = "Fornite", RequiredLvl = "Level 6", Description = "https://www.assassinscreed.com" });
-            items.Add(new Games() { Dates = "29/12/1999", Name = "Battlefield", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè " });
+            items.Add(new Games() { Dates = "29/12/1999", Name = "Battlefield", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè" });
             items.Add(new Games() { Dates = "29/12/1999", Name = "Dota", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè " });
             items.Add(new Games() { Dates = "29/12/1999", Name = "Counter-Strike : Global Offensive", RequiredLvl = "Level 6", Description = "qweqwádfádf ăè ăè adfăè ăe ăè " });
 
