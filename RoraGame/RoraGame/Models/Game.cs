e603a9779fa540701e9d44c24bcd90b11e2c3506
anhmairora <anhmai.rora@gmail.com>
2019-03-02 17:51:27 +0700

    Optimize game logic

--- RoraGame/RoraGame/Models/Game.cs
+++ RoraGame/RoraGame/Models/Game.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace RoraGame.Models
 {
     class Game
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public string RequiredLvl { get; set; }
+        public string GameCode { get; set; }
+        public string Description { get; set; }
+
+        #region METHOD
+
+
+
+        #endregion
+
+        #region API SERVICES
+
+        public static List<Game> getGameList()
+        {
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:57677/");
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/GameList").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var gameList = response.Content.ReadAsAsync<List<Game>>().Result;
+                return gameList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        #endregion
     }
 }
