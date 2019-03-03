a24fc4d1d321d80f77192974150d69c4dde559b4
hoangharora <hoangha.rora@gmail.com>
2019-03-03 17:17:36 +0700

    Fix Cannot Run Steam Except C Drive

--- RoraGame/RoraGame/Views/GameList/GameListUCViewModel.cs
+++ RoraGame/RoraGame/Views/GameList/GameListUCViewModel.cs
@@ -61,7 +61,8 @@ namespace RoraGame.Views.UserControls.GameList
                 #region Login Steam
                 case "Steam":
                     string LoginSteam;
-                    LoginSteam = @"/c cd " + folderSteam + " && start steam.exe -login " + steamUsername + " " + steamPassword;
+                    //LoginSteam = @"/c cd " + folderSteam + " && start steam.exe -login " + steamUsername + " " + steamPassword;
+                    LoginSteam = "/c start \"\" \"C:\\Program Files (x86)\\Steam\\steam.exe\" -login pubgvna_2875 Pubgvna123123";
                     Process p = new Process();
                     p.StartInfo.FileName = "CMD.exe";
                     p.StartInfo.Arguments = LoginSteam;
